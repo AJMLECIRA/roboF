12b08481e6ff5e1f442035dcd4eef2897833bdad
Andrew Mayers <andrew@mirrorworld.co.uk>
2023-07-07 17:27:55 +0100

    First Update

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "andrew",
+    username: "AJM",
+    email: "andrew@mirrorworld.co.uk",
+  },
+  {
+    id: 2,
+    name: "Jill",
+    username: "Jilly",
+    email: "jill@parkwood.eclipse.co.uk",
+  },
+  {
+    id: 3,
+    name: "Harry",
+    username: "Hazard",
+    email: "harry@mirrorworld.co.uk",
+  },
+  {
+    id: 4,
+    name: "Jacob",
+    username: "J",
+    email: "jacob@parkwood.eclipse.co.uk",
+  },
+  {
+    id: 5,
+    name: "Gareth",
+    username: "Sales",
+    email: "sales@mirrorworld.co.uk",
+  },
+  {
+    id: 6,
+    name: "James",
+    username: "Production",
+    email: "Production@mirrorworld.co.uk",
+  },
+  {
+    id: 7,
+    name: "Tony",
+    username: "Dispatch",
+    email: "logistics@mirrorworld.co.uk",
+  },
+  {
+    id: 8,
+    name: "Kurt",
+    username: "Tools",
+    email: "logistics@mirrorworld.co.uk",
+  },
+  {
+    id: 9,
+    name: "Mark",
+    username: "Woodwork",
+    email: "logistics@mirrorworld.co.uk",
+  },
+  {
+    id: 10,
+    name: "Lee",
+    username: "Driver",
+    email: "logistics@mirrorworld.co.uk",
+  },
+];
